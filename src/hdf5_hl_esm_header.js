6cac56d624d1e089597f89d774992388952d3e7a
Brian Ben Maranville <brian.maranville@nist.gov>
2022-01-20 00:42:03 -0500

    working version

--- src/hdf5_hl_esm_header.js
+++
@@ -1,8 +0,0 @@
-import ModuleFactory from './esm/hdf5_util.js';
-export {File, Group, Dataset, ready, FS, Module, ACCESS_MODES};
-
-var Module = null;
-var FS = null;
-const ready = ModuleFactory({ noInitialRun: true }).then((result) => { Module = result; FS = Module.FS });
-
-
--- src/hdf5_hl_node_footer.js
+++
@@ -1,2 +0,0 @@
-const Module = require("./node/hdf5_util.js");
-module.exports = {File, Dataset, Group, ACCESS_MODES};
