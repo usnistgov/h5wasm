97dd844eb2005abddd03af9979c8802880990aee
Loic Huder <loic.huder@esrf.fr>
2022-05-10 10:25:44 +0200

    Add test for boolean dataset

--- test/test.mjs
+++ test/test.mjs
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
+import bool_test from "./bool_test.mjs"
 // tests for H5T_COMPOUND and H5T_ARRAY support
 import compound_and_array_tests from "./compound_and_array_test.mjs";
 import create_dataset from './create_group_dataset.mjs';
 
 let tests = [];
-let add_tests = (tests_in) => { /*global*/ tests = tests.concat(tests_in)}
+const add_tests = (tests_in) => { /*global*/ tests = tests.concat(tests_in)}
 
+add_tests(bool_test);
 add_tests(compound_and_array_tests);
 add_tests(create_dataset);
 
