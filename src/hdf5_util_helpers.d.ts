96ccf7e0e188b6995ebcfc24bf31fa3fd064a207
Brian Ben Maranville <brian.maranville@nist.gov>
2022-08-04 13:21:52 -0400

    remove refresh argument from get_dataset_metadata, implement separate dataset_refresh

--- src/hdf5_util_helpers.d.ts
+++ src/hdf5_util_helpers.d.ts
@@ -69,7 +69,8 @@ export interface H5Module extends EmscriptenModule {
     create_group(file_id: bigint, name: string): number;
     create_vlen_str_dataset(file_id: bigint, dset_name: string, prepared_data: any, arg3: any, type: number, size: number, signed: boolean, vlen: boolean): number;
     get_dataset_data(file_id: bigint, path: string, arg2: bigint[] | null, arg3: bigint[] | null, arg4: bigint): number;
-    get_dataset_metadata(file_id: bigint, path: string, refresh: boolean): Metadata;
+    refresh_dataset(file_id: bigint, path: string): number;
+    get_dataset_metadata(file_id: bigint, path: string): Metadata;
     flush(file_id: bigint): number;
     ccall: typeof ccall;
     get_names(file_id: bigint, path: string): string[];
