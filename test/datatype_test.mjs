21d296cf83b48e9dfe613e042f0fc619f79be358
bbm <brian.maranville@nist.gov>
2022-09-19 10:50:13 -0400

    fix Datatype test

--- test/datatype_test.mjs
+++ test/datatype_test.mjs
@@ -7,9 +7,7 @@ async function datatype_test() {
   await h5wasm.ready;
   var f = new h5wasm.File('./test/array.h5', 'r');
 
-  assert.deepEqual(f.get('datatype/value'), {
-    type: 'Datatype',
-  });
+  assert.deepEqual(f.get('datatype/value'), new h5wasm.Datatype());
 }
 
 export const tests = [
